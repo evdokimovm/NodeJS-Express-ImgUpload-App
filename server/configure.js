96a9357e7859d57e1dcacc719d2a9d08baed674d
evdokimovm <tel89147925532@gmail.com>
2015-11-18 00:28:21 +1000

    Migrating to Express v4.0.0

--- server/configure.js
+++ server/configure.js
@@ -1,7 +1,17 @@
-var connect = require('connect'),
-	path = require('path'),
+// var connect = require('connect'),
+// 	path = require('path'),
+// 	routes = require('./routes'),
+// 	exphbs = require('express3-handlebars');
+
+var path = require('path'),
 	routes = require('./routes'),
-	exphbs = require('express3-handlebars');
+	exphbs = require('express3-handlebars'),
+	express = require('express'),
+	bodyParser = require('body-parser'),
+	cookieParser = require('cookie-parser'),
+	morgan = require('morgan'),
+	methodOverride = require('method-override'),
+	errorHandler = require('errorhandler');
 
 module.exports = function(app) {
 	// configuration code...
@@ -13,22 +23,31 @@ module.exports = function(app) {
 	}).engine);
 	app.set('view engine', 'handlebars');
 
-	app.use(connect.logger('dev'));
-	app.use(connect.bodyParser({
+	// app.use(connect.logger('dev'));
+	app.use(morgan('dev'));
+	// app.use(connect.bodyParser({
+	// 	uploadDir:path.join(__dirname, '../public/upload/temp')
+	// }));
+	// app.use(connect.json());
+	// app.use(connect.urlencoded());
+	app.use(bodyParser({
 		uploadDir:path.join(__dirname, '../public/upload/temp')
 	}));
-	app.use(connect.json());
-	app.use(connect.urlencoded());
-	app.use(connect.methodOverride());
-	app.use(connect.cookieParser('some-secret-value-here'));
-	app.use(app.router);
-	app.use('/public/', connect.static(path.join(__dirname, '../public')));
+	// app.use(connect.methodOverride());
+	app.use(methodOverride());
+	// app.use(connect.cookieParser('some-secret-value-here'));
+	app.use(cookieParser('some-secret-value-here'));
+	// app.use(app.router);
+	routes.initialize(app, new express.Router());
+	// app.use('/public/', connect.static(path.join(__dirname, '../public')));
+	app.use('/public/', express.static(path.join(__dirname, '../public')));
 
 	if ('development' === app.get('env')) {
-		app.use(connect.errorHandler());
+		// app.use(connect.errorHandler());
+		app.use(errorHandler());
 	}
 
-	routes.initialize(app);
+	// routes.initialize(app);
 
 	return app;
 };
